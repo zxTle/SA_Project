8a6a5a76656d8e7fbd3ae17729987f47b2baadb7
chutikarn <chutikarn.mo@ku.th>
2021-10-19 20:28:58 +0700

    Update Home

--- src/main/java/sa_project/Main.java
+++ src/main/java/sa_project/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,6 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/Home.fxml"));
+        root.getStylesheets().add("https://fonts.googleapis.com/css2?family=Barlow&family=Kanit:wght@200;300;400&display=swap");
         primaryStage.setTitle("MICRO SCIENCE");
         primaryStage.setScene(new Scene(root, 1280, 768));
         primaryStage.show();
