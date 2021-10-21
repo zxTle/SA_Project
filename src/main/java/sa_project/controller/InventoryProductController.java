7e1d00c53a6e617e711607db064a5dedb28edbc2
chutikarn <chutikarn.mo@ku.th>
2021-10-21 21:32:26 +0700

    Update page & table css

--- src/main/java/sa_project/controller/InventoryProductController.java
+++ src/main/java/sa_project/controller/InventoryProductController.java
@@ -1,4 +1,35 @@
 package sa_project.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import sa_project.model.Products;
+
 public class InventoryProductController {
+//    @FXML
+//    private TableView<Products> productTable;
+//    @FXML private TableColumn<Products,String> productCodeTb;
+//    @FXML private TableColumn<Products,String> productNameTb;
+//    @FXML private TableColumn<Products,String> productSpecTb;
+//    @FXML private TableColumn<Products,Integer> productInTb;
+//    @FXML private TableColumn<Products,Integer> productWantTb;
+//    @FXML private TableColumn<Products,Integer> productAvailTb;
+//
+//    ObservableList<Products> list = FXCollections.observableArrayList(
+//        new Products("CAM001","Camera1245","ppppppppp",2,3,4)
+//    );
+//
+//    public void initialize(){
+//        productCodeTb.setCellValueFactory(new PropertyValueFactory<Products, String>("productCode"));
+//        productNameTb.setCellValueFactory(new PropertyValueFactory<Products, String>("productName"));
+//        productSpecTb.setCellValueFactory(new PropertyValueFactory<Products, String>("productSpec"));
+//        productInTb.setCellValueFactory(new PropertyValueFactory<Products, Integer>("quantityIn"));
+//        productWantTb.setCellValueFactory(new PropertyValueFactory<Products, Integer>("quantityWant"));
+//        productAvailTb.setCellValueFactory(new PropertyValueFactory<Products, Integer>("quantityAvailable"));
+//
+//        productTable.setItems(list);
+//    }
 }
