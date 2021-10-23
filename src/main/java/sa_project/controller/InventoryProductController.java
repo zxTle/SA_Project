d1dad6fd9e1f554533f105e728927ab98dc0cac2
chutikarn <chutikarn.mo@ku.th>
2021-10-23 13:07:58 +0700

    InventoryProducts

--- src/main/java/sa_project/controller/InventoryProductController.java
+++ src/main/java/sa_project/controller/InventoryProductController.java
@@ -2,22 +2,43 @@ package sa_project.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import sa_project.model.Products;
 
+
+
 public class InventoryProductController {
-//    @FXML
-//    private TableView<Products> productTable;
-//    @FXML private TableColumn<Products,String> productCodeTb;
-//    @FXML private TableColumn<Products,String> productNameTb;
-//    @FXML private TableColumn<Products,String> productSpecTb;
-//    @FXML private TableColumn<Products,Integer> productInTb;
-//    @FXML private TableColumn<Products,Integer> productWantTb;
-//    @FXML private TableColumn<Products,Integer> productAvailTb;
-//
+    @FXML private Label dateLabel,usernameLabel,productCode,productType;
+    @FXML private Button listRQBtn,createRQBtn,logoutBtn,createRQBtn1,EditBtn,BackBtn,searchBtn;
+    @FXML private TextField inputSearch,productName;
+    @FXML private TextArea productSpec;
+//    private TableView<?> productTable;
+//    private TableColumn<?, ?> productCodeTb;
+//    private TableColumn<?, ?> productNameTb;
+//    private TableColumn<?, ?> productSpecTb;
+//    private TableColumn<?, ?> productInTb;
+//    private TableColumn<?, ?> productWantTb;
+//    private TableColumn<?, ?> productAvailTb;
+    @FXML private ImageView icon;
+
+
+    public void initialize(){}
+    @FXML
+    private void handleEditPageClick(ActionEvent click){
+        if(click.getSource() == EditBtn){
+            productName.setDisable(false);
+            productSpec.setDisable(false);
+            EditBtn.setText("บันทึก");
+            Image ic = new Image("image/file-download-solid.png");
+            icon.setImage(ic);
+        }
+    }
+
 //    ObservableList<Products> list = FXCollections.observableArrayList(
 //        new Products("CAM001","Camera1245","ppppppppp",2,3,4)
 //    );
