cc39cca5e80358aa1ec4e36996933b79231a58de
Pupae-ktsw <punthita.kh@ku.th>
2021-10-27 18:07:37 +0700

    StockOut finish
    fxml ใบเบิกของ รวบในหน้าเดียวหมดแล้ว

--- src/main/java/sa_project/Main.java
+++ src/main/java/sa_project/Main.java
@@ -11,10 +11,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/InventoryAllReceive.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("/InventoryAllReceive.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/StockOut.fxml"));
         root.getStylesheets().add("https://fonts.googleapis.com/css2?family=Barlow&family=Kanit:wght@200;300;400&display=swap");
         primaryStage.setTitle("MICRO SCIENCE");
         primaryStage.setScene(new Scene(root, 1280, 768));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
